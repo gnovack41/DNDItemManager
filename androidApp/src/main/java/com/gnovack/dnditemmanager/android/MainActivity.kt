cbe7dea86a158359a49f4f0a38ef5fb9e12ea82a
Gabriel Novack <45369167+gnovack41@users.noreply.github.com>
2024-07-19 13:06:43 -0400

    Initial commit

---
+++ androidApp/src/main/java/com/gnovack/dnditemmanager/android/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.gnovack.dnditemmanager.android
+
+import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.gnovack.dnditemmanager.Greeting
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Surface(Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.surface) {
+                    GreetingView(Greeting().greet())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingView(text: String) {
+    Text(text = text)
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+        GreetingView("Hello, Android!")
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/gnovack/dnditemmanager/Greeting.kt
@@ -0,0 +1,9 @@
+package com.gnovack.dnditemmanager
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
